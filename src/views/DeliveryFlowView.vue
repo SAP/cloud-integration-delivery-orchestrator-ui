6827b27e87e056fe89817d6ecf7ba63504500c5d
I589335 <doug.liu@sap.com>
2025-12-07 23:13:15 +0800

    Composition API

--- src/views/DeliveryFlowView.vue
+++ src/views/DeliveryFlowView.vue
@@ -11,155 +11,151 @@
         fit-view-on-init
     >
         <template #node-deliver-group="props">
-            <DeliverGroupNode v-bind="props" :id="props.id" :data="props.data" @deliver="onDeliver" @import-only="onImportOnly" @deploy-only="onDeployOnly"/>
+            <DeliverGroupNode v-bind="props" @deliver="onDeliver" @import-only="onImportOnly" @deploy-only="onDeployOnly"/>
         </template>
     </VueFlow>
 
 </template>
 
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue'
 import { VueFlow, type Edge, type Node } from '@vue-flow/core'
-import type { ArtifactTenantOperation, CpiTenant, DeliveryRequest, TransportNode, TransportRoute } from '@/service/model';
-import { DeployOps, ImportOps, layoutNodes } from '@/service/api';
-import DeliverGroupNode from '@/components/DeliverGroupNode.vue';
+import type { ArtifactTenantOperation, CpiTenant, DeliveryRequest } from '@/service/model'
+import { DeployOps, ImportOps, layoutNodes } from '@/service/api'
+import DeliverGroupNode from '@/components/DeliverGroupNode.vue'
 
-export default defineComponent({
-    components: { VueFlow, DeliverGroupNode },
-    props: {
-        deliveryRequest: {
-            type: Object as () => DeliveryRequest,
-            required: true
-        },
-        cpiTenants: { // all cpi tenants
-            type: Array as () => CpiTenant[],
-            required: true
-        },
-        tenantToOps: { // map[tenantID]map[trNumber]ArtifactTenantOperation
-            type: Object as () => {[key: number] : {[key: string]: ArtifactTenantOperation}},
-            required: true
-        }
-    },
-    computed: {
-        nodeToTenant(): {[key: number]: CpiTenant} { // transport node ID to cpi tenant
-            const cache: {[key: number]: CpiTenant} = {}
-            this.cpiTenants.forEach(opt => cache[opt.TransportNodeID] = opt)
-            return cache
-        },
-        childNodes() { // parentNodeId - childNodeIds[]
-            const childNodes: {[key: number]: number[]} = {}
-            this.deliveryRequest?.DeliveryRule.TargetRoutes?.forEach(tRoute => {
-                const {targetNodeId, sourceNodeId} = tRoute
-                if(!childNodes[sourceNodeId]) childNodes[sourceNodeId] = []
-                childNodes[sourceNodeId].push(targetNodeId)
-            })
-            return childNodes
-        },
-        toGroupNode(): {[key: number]: Node} { // transport node ID -> (group) node. group many transport nodes into one node, based on parent-child relationship
-            const tenantGroups: {[key: string]: CpiTenant[]} = {} // group label -> tenants[]
-            this.deliveryRequest?.DeliveryRule?.TargetNodes?.forEach(n => {
-                const t = this.nodeToTenant[n.id]
-                if(!t) return
-                const groupLabel = t.Group || t.Name
-                if(!tenantGroups[groupLabel]) tenantGroups[groupLabel] = []
-                tenantGroups[groupLabel].push(t)
-            })
-            const groupNodeMap: {[key: number]: Node} = {} // many NodeId -> Node. 
-            Object.entries(tenantGroups).forEach(([groupLabel, tenants]) => {
-                const isSource = tenants.some(t => this.deliveryRequest?.SourceTenant && t.ID === this.deliveryRequest.SourceTenant.ID)
-                const isTail = tenants.map(t => !this.childNodes[t.TransportNodeID] || this.childNodes[t.TransportNodeID].length === 0).every(v => v)
-                const groupNode: Node = {
-                    id: `n-group-${groupLabel}`,
-                    data: { 
-                        label: groupLabel, 
-                        sourceNodeId: 0, 
-                        tenants: tenants, 
-                        isSource: isSource, 
-                        isTail: isTail,
-                        tenantToOps: this.tenantToOps
-                    },
-                    position: { x: 0, y: 0 }, // Placeholder position
-                    type: 'deliver-group',
-                    width: 300,
-                    height: 150,
-                }
-                tenants.forEach(t => {groupNodeMap[t.TransportNodeID] = groupNode})
-            })
-            return groupNodeMap
-        },
-        toEdge() { // key: group node id: `e-(${pGroupNode.id})-to-(${cGrouNpde.id})`. value: Edge
-            const edgeMap: {[key: string]: Edge} = {}
-            Object.keys(this.toGroupNode).forEach(nodeIDStr => {
-                const pNodeID = Number(nodeIDStr)
-                const childNodeIDs = this.childNodes[pNodeID] || []
-                const pGroupNode = this.toGroupNode[pNodeID]
-                childNodeIDs.forEach(childNodeID => {
-                    const cGrouNpde = this.toGroupNode[childNodeID]
-                    const id = `e-(${pGroupNode.id})-to-(${cGrouNpde.id})`
-                    if (edgeMap[id]) return // skip if edge already exists
-                    edgeMap[id] = {
-                        id: id,
-                        source: pGroupNode.id,
-                        target: cGrouNpde.id,
-                        animated: true,
-                        style: { strokeWidth: 3}
-                    }
-                })
-            })
-            return edgeMap
-        },
-        graph() {
-            const uniqueNodes: {[key: string]: Node} = {}
-            Object.values(this.toGroupNode).forEach(n => {
-                if (!(n.id in uniqueNodes)) uniqueNodes[n.id] = n
-            })
-            const nodes = Object.values(uniqueNodes)
+// Props
+const props = defineProps<{
+    deliveryRequest: DeliveryRequest
+    cpiTenants: CpiTenant[]
+    tenantToOps: { [key: number]: { [key: string]: ArtifactTenantOperation } }
+}>()
 
-            const uniqueEdges: {[key: string]: Edge} = {}
-            Object.values(this.toEdge).forEach(e => {
-                if (!(e.id in uniqueEdges)) uniqueEdges[e.id] = e
-            })
-            const edges = Object.values(uniqueEdges)
-            return layoutNodes(nodes, edges)
-
-        }
-    },
-    methods: {
-        groupLabel(childNodeIds: number[]): string {
-            if(childNodeIds.length === 1) {
-                return this.nodeToTenant[childNodeIds[0]]?.Name || `node-${childNodeIds[0]}`
-            }
-            const labelIdx = childNodeIds.findIndex(nodeID => this.nodeToTenant[nodeID]?.Group) // find the first group label as label
-            return labelIdx >=0 ? this.nodeToTenant[childNodeIds[labelIdx]].Group : `group-${this.nodeToTenant[childNodeIds[0]]?.Name}`
-        },
-        groupKey(parentNodeID: number, childNodeIds: number[]): string {
-            const cTenants = childNodeIds.map(id => this.nodeToTenant[id]?.Name).join(', ')
-            return `n-group-${cTenants}-from-${this.nodeToTenant[parentNodeID]?.Name}`
-        },
-        async onDeliver(payload: { tenantIDs: number[] }) {
-            await this.onImportOnly(payload)
-            await this.onDeployOnly(payload)
-        },
-        async onImportOnly(payload: { tenantIDs: number[] }) {
-            const tasks: Promise<any>[] = []
-            payload.tenantIDs.forEach(tID => {
-                const ops = Object.values(this.tenantToOps[tID]).map(op => op.ID)
-                tasks.push(ImportOps(ops, tID))
-            })
-            await Promise.all(tasks)
-        },
-        async onDeployOnly(payload: { tenantIDs: number[] }) {
-            const tasks: Promise<any>[] = []
-            payload.tenantIDs.forEach(tID => {
-                const ops = Object.values(this.tenantToOps[tID]).map(op => op.ID)
-                tasks.push(DeployOps(ops, tID))
-            })
-            await Promise.all(tasks)
-        }
-    }
-
-    
+// transport node ID to cpi tenant
+const nodeToTenant = computed<{ [key: number]: CpiTenant }>(() => {
+    const cache: { [key: number]: CpiTenant } = {}
+    props.cpiTenants.forEach(opt => { cache[opt.TransportNodeID] = opt })
+    return cache
 })
+
+// parentNodeId - childNodeIds[]
+const childNodes = computed<{ [key: number]: number[] }>(() => {
+    const child: { [key: number]: number[] } = {}
+    props.deliveryRequest?.DeliveryRule.TargetRoutes?.forEach(tRoute => {
+        const { targetNodeId, sourceNodeId } = tRoute
+        if (!child[sourceNodeId]) child[sourceNodeId] = []
+        child[sourceNodeId].push(targetNodeId)
+    })
+    return child
+})
+
+// transport node ID -> (group) node
+const toGroupNode = computed<{ [key: number]: Node }>(() => {
+    const tenantGroups: { [key: string]: CpiTenant[] } = {}
+    props.deliveryRequest?.DeliveryRule?.TargetNodes?.forEach(n => {
+        const t = nodeToTenant.value[n.id]
+        if (!t) return
+        const groupLabel = t.Group || t.Name
+        if (!tenantGroups[groupLabel]) tenantGroups[groupLabel] = []
+        tenantGroups[groupLabel].push(t)
+    })
+    const groupNodeMap: { [key: number]: Node } = {}
+    Object.entries(tenantGroups).forEach(([groupLabel, tenants]) => {
+        const isSource = tenants.some(t => props.deliveryRequest?.SourceTenant && t.ID === props.deliveryRequest.SourceTenant.ID)
+        const isTail = tenants.map(t => !childNodes.value[t.TransportNodeID] || childNodes.value[t.TransportNodeID].length === 0).every(v => v)
+        const groupNode: Node = {
+            id: `n-group-${groupLabel}`,
+            data: {
+                label: groupLabel,
+                sourceNodeId: 0,
+                tenants: tenants,
+                isSource: isSource,
+                isTail: isTail,
+                tenantToOps: props.tenantToOps
+            },
+            position: { x: 0, y: 0 },
+            type: 'deliver-group',
+            width: 300,
+            height: 150,
+        }
+        tenants.forEach(t => { groupNodeMap[t.TransportNodeID] = groupNode })
+    })
+    return groupNodeMap
+})
+
+// key: group node id -> Edge
+const toEdge = computed<{ [key: string]: Edge }>(() => {
+    const edgeMap: { [key: string]: Edge } = {}
+    Object.keys(toGroupNode.value).forEach(nodeIDStr => {
+        const pNodeID = Number(nodeIDStr)
+        const childNodeIDs = childNodes.value[pNodeID] || []
+        const pGroupNode = toGroupNode.value[pNodeID]
+        childNodeIDs.forEach(childNodeID => {
+            const cGrouNpde = toGroupNode.value[childNodeID]
+            const id = `e-(${pGroupNode.id})-to-(${cGrouNpde.id})`
+            if (edgeMap[id]) return
+            edgeMap[id] = {
+                id,
+                source: pGroupNode.id,
+                target: cGrouNpde.id,
+                animated: true,
+                style: { strokeWidth: 3 }
+            }
+        })
+    })
+    return edgeMap
+})
+
+const graph = computed(() => {
+    const uniqueNodes: { [key: string]: Node } = {}
+    Object.values(toGroupNode.value).forEach(n => {
+        if (!(n.id in uniqueNodes)) uniqueNodes[n.id] = n
+    })
+    const nodes = Object.values(uniqueNodes)
+
+    const uniqueEdges: { [key: string]: Edge } = {}
+    Object.values(toEdge.value).forEach(e => {
+        if (!(e.id in uniqueEdges)) uniqueEdges[e.id] = e
+    })
+    const edges = Object.values(uniqueEdges)
+    return layoutNodes(nodes, edges)
+})
+
+function groupLabel(childNodeIds: number[]): string {
+    if (childNodeIds.length === 1) {
+        return nodeToTenant.value[childNodeIds[0]]?.Name || `node-${childNodeIds[0]}`
+    }
+    const labelIdx = childNodeIds.findIndex(nodeID => nodeToTenant.value[nodeID]?.Group)
+    return labelIdx >= 0 ? nodeToTenant.value[childNodeIds[labelIdx]].Group : `group-${nodeToTenant.value[childNodeIds[0]]?.Name}`
+}
+
+function groupKey(parentNodeID: number, childNodeIds: number[]): string {
+    const cTenants = childNodeIds.map(id => nodeToTenant.value[id]?.Name).join(', ')
+    return `n-group-${cTenants}-from-${nodeToTenant.value[parentNodeID]?.Name}`
+}
+
+async function onDeliver(payload: { tenantIDs: number[] }) {
+    await onImportOnly(payload)
+    await onDeployOnly(payload)
+}
+
+async function onImportOnly(payload: { tenantIDs: number[] }) {
+    const tasks: Promise<any>[] = []
+    payload.tenantIDs.forEach(tID => {
+        const ops = Object.values(props.tenantToOps[tID]).map(op => op.ID)
+        tasks.push(ImportOps(ops, tID))
+    })
+    await Promise.all(tasks)
+}
+
+async function onDeployOnly(payload: { tenantIDs: number[] }) {
+    const tasks: Promise<any>[] = []
+    payload.tenantIDs.forEach(tID => {
+        const ops = Object.values(props.tenantToOps[tID]).map(op => op.ID)
+        tasks.push(DeployOps(ops, tID))
+    })
+    await Promise.all(tasks)
+}
 
 </script>
