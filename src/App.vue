922513c0c87b87df089ea7d9476e44a368d55eba
I589335 <doug.liu@sap.com>
2025-12-15 23:17:32 +0800

    refactor: simplify routing methods and streamline user profile handling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { ref, computed, onMounted, useTemplateRef, type Ref } from 'vue'
 
 import { useRouter } from 'vue-router'
-import { IosArrowBack } from '@vicons/ionicons4'
 import MessageItem from '@/components/MessageComp.vue'
 import { CurrentUser } from './service/api'
 import "@ui5/webcomponents-fiori/dist/ShellBar.js"
@@ -19,27 +18,14 @@ import "@ui5/webcomponents/dist/List.js";
 import "@ui5/webcomponents/dist/ListItemStandard.js";
 // Router
 const router = useRouter()
-// Expose as `route` to keep existing template usage (route.currentRoute.name)
-const route = router
 
-// State
 const userInfo = ref<{ [key: string]: string }>({})
-
 const openProfile = ref(false)
 
 // Lifecycle: load current user
 onMounted(async () => {
   userInfo.value = await CurrentUser()
 })
-
-// Methods
-function handleBack() {
-  router.go(-1)
-}
-
-function handleHome() {
-  router.push('/')
-}
 
 function handleLogout() {
   window.location.href = '/logout'
@@ -51,19 +37,21 @@ const avatarInit = computed(
   () => `${userInfo.value.firstname?.charAt(0) || ''}${userInfo.value.lastname?.charAt(0) || ''}`
 )
 const userEmail = computed(() => userInfo.value.email)
-
 const userName = computed(() => `${userInfo.value.firstname || ''} ${userInfo.value.lastname || ''}`)
 </script>
 
 <template>
   <ui5-shellbar>
-    <ui5-shellbar-branding slot="branding">
+    <ui5-shellbar-branding @click="() => router.push('/')" slot="branding">
       MMT DevOps
       <img slot="logo" src="https://ui5.github.io/webcomponents/images/sap-logo-svg.svg" />
     </ui5-shellbar-branding>
-    <ui5-button v-if="canBack" @click="handleBack" icon="nav-back" slot="startButton"></ui5-button>
+    <ui5-button v-if="canBack" @click="() => router.go(-1)" icon="nav-back" slot="startButton"></ui5-button>
     <ui5-shellbar-item icon="sys-help" text="Help"></ui5-shellbar-item>
-    <ui5-avatar @click="() => { openProfile = !openProfile }" id="pop-user-profile" slot="profile"
+    <ui5-avatar 
+      @click="() => { openProfile = !openProfile }" 
+      id="pop-user-profile" 
+      slot="profile"
       :initials="avatarInit"></ui5-avatar>
   </ui5-shellbar>
 
